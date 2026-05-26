fa8d03ea6a06a18b242aa8ade65edef5759d07fd
shuichi2828 <Shuichi20061207@icloud.com>
2026-05-26 16:05:21 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/tournament-bracket/",
+  base: "/tournament-maker/",
   plugins: [react(), tailwindcss()],
 });
